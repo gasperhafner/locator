3f9466bd3f9d20a9123b2e69c07a6150f317312a
Gasper Hafner <gasper.hafner@dlabs.si>
2018-10-29 20:29:52 +0100

    Sidekiq config.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,6 +10,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       execute "sudo systemctl restart rails.service"
+      execute "sudo systemctl restart sidekiq.service"
       #execute "sudo /usr/bin/monit restart sidekiq_staging_laundryheap_com"
     end
   end
