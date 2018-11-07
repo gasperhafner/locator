1e8351cc907c37bcc6df03f0470ce66f9987fc39
Gasper Hafner <gasper.hafner@dlabs.si>
2018-11-07 09:26:53 +0100

    Message-ID

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
   default from: "no-reply@poisci.ga"
+  default "Message-ID" => "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@poisci.ga"
 
   def confirmation(user)
     @url = "https://poisci.ga/confirmation?token=#{user.confirmation_token}"
